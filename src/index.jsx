83ef5582f50a7b5a52bcd3403c1788f9b43cc5ab
dasuttam05-netizen <das.uttam05@gmail.com>
2026-05-14 16:48:45 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -20,8 +20,4 @@ axios.interceptors.request.use((config) => {
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
