548322bd47156b4dcb45b19396bc50eb130cd072
dasuttam05-netizen <das.uttam05@gmail.com>
2026-06-18 15:27:54 +0530

    Update axiosInstance.js

--- src/pages/axiosInstance.js
+++ src/pages/axiosInstance.js
@@ -7,7 +7,10 @@ const API = axios.create({
 
 API.interceptors.request.use(config => {
   const token = localStorage.getItem("token");
-  if(token) config.headers["Authorization"] = `Bearer ${token}`;
+  config.headers = config.headers || {};
+  if (token) {
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
   return config;
 });
 
