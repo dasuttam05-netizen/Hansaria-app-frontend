08e5bed6b10469b9a5792a1ba9ba6958a6dd0b81
dasuttam05-netizen <das.uttam05@gmail.com>
2026-08-30 22:05:38 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import axios from "axios";
-import App from "./App";
+import App from "./App.js";
 import { loadSession, touchSessionActivity } from "./utils/auth";
 import "./mobile.css";
 
