f5c0f823cfe387bf3cb3ffd76a8f80c43d47f85f
dasuttam05-netizen <das.uttam05@gmail.com>
2026-09-02 22:54:52 +0530

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "hansaria-pwa-v6";
+const CACHE_NAME = "hansaria-pwa-v7";
 const APP_SHELL = ["/", "/manifest.json", "/app-icon.svg"];
 
 self.addEventListener("install", (event) => {
@@ -45,8 +45,10 @@ self.addEventListener("fetch", (event) => {
     event.respondWith(
       fetch(event.request)
         .then((response) => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put("/", copy));
+          if (response.ok) {
+            const copy = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put("/", copy));
+          }
           return response;
         })
         .catch(() => caches.match("/") || caches.match(event.request))
